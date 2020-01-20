733da562bdda9702a25aa52be33b48ad7a0ce4c3
Mochamad Faishal Amir <42466206+mochfamir@users.noreply.github.com>
2020-01-20 09:48:05 +0700

    remove credential config

--- projects/todo/src/environments/environment.prod.ts
+++ projects/todo/src/environments/environment.prod.ts
@@ -1,15 +1,4 @@
-const firebaseConfig = {
-  apiKey: 'AIzaSyCEkMHrUo5o5iw3aYeswWZMC9bxly9PQzc',
-  authDomain: 'angular-apps-faishal.firebaseapp.com',
-  databaseURL: 'https://angular-apps-faishal.firebaseio.com',
-  projectId: 'angular-apps-faishal',
-  storageBucket: 'angular-apps-faishal.appspot.com',
-  messagingSenderId: '408010100536',
-  appId: '1:408010100536:web:1aea427f9118b818b67bcd',
-  measurementId: 'G-2JDC4G27NW'
-};
 
 export const environment = {
-  production: true,
-  firebaseConfig
+  production: true
 };
